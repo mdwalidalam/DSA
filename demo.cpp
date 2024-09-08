5690b000eefdc2ec09744da360d2a3a862ea1261
mdwalidalam600 <mdwalidalam80@gmail.com>
2024-09-08 22:48:08 +0600

    added all

---
+++ demo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int a = 10, b = 5;
+int sum = a+b;
+
+cout << "sum =" << (a+b) << "\ndifferance = " << (a-b) << "\nproduct =" << (a*b) << "\ndivition = " << (a/b) << "\nmodulus =" << a%b << endl;
+return 0;
+
+}
---
+++ demo2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int a, b;
+cout << "enter a : ";
+cin >> a;
+
+cout << "enter b :";
+cin >> b;
+int sum = a+b;
+cout << "sum = " << sum << endl;
+int sub = a-b;
+cout << "sub = " << sub << endl;
+int mul = a*b;
+cout << "mul = " << mul << endl;
+int div = a/b;
+cout << "div = " << div << endl;
+int mod = a%b;
+cout << "mod = " << mod << endl;
+return 0;
+
+}
