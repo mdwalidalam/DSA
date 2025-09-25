6d4f9d736f59b0121cbd9ca03cf68ac748524a8c
MD: WALID ALAM <155137314+mdwalidalam@users.noreply.github.com>
2025-09-25 12:42:17 +0600

    Time: 14 ms (61.19%) | Memory: 9.4 MB (81.44%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if (n == 0) return "";
+        int start = 0, maxLen = 1;
+
+        auto expand = [&](int l, int r) {
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > maxLen) {
+                    start = l;
+                    maxLen = r - l + 1;
+                }
+                l--;
+                r++;
+            }
+        };
+
+        for (int i = 0; i < n; i++) {
+            expand(i, i);     // odd length
+            expand(i, i + 1); // even length
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
